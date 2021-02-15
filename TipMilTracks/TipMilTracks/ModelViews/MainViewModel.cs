1539281db9e5dcde48abe7c4ef776243000c51a5
Marco Preciado <mac46005@gmail.com>
2021-02-15 16:36:50 -0600

    MVVM Configuration

---
+++ TipMilTracks/TipMilTracks/ModelViews/MainViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TipMilTracks.ModelViews
+{
+    public class MainViewModel
+    {
+    }
+}
--- TipMilTracks/TipMilTracks/Views/MainView.xaml.cs
+++ TipMilTracks/TipMilTracks/Views/MainView.xaml.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Xamarin.Forms;
 
-namespace TipMilTracks
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TipMilTracks.Views
 {
-    public partial class MainPage : ContentPage
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainView : ContentPage
     {
-        public MainPage()
+        public MainView()
         {
             InitializeComponent();
         }
