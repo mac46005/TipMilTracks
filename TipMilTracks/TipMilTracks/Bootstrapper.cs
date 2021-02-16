a14d860bd43e7f52fadd3453fd3119c31377ea9f
Marco Preciado <mac46005@gmail.com>
2021-02-15 20:42:40 -0600

    continue configuring bootstrapper

--- TipMilTracks/TipMilTracks/Bootstrapper.cs
+++ TipMilTracks/TipMilTracks/Bootstrapper.cs
@@ -1,23 +1,38 @@
-﻿using System;
+﻿using Autofac;
+using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text;
+using System.Linq;
+using Xamarin.Forms;
+using TipMilTracks.ModelViews;
 
 namespace TipMilTracks
 {
     public abstract class Bootstrapper
     {
+        protected ContainerBuilder ContainerBuilder { get; set; }
         public Bootstrapper()
         {
             Initialize();
             FinishInitialization();
         }
 
-        private void FinishInitialization()
-        {
-            throw new NotImplementedException();
-        }
+        
 
         private void Initialize()
+        {
+            var currentAssembly = Assembly.GetExecutingAssembly();
+            ContainerBuilder = new ContainerBuilder();
+            foreach (var type in currentAssembly.DefinedTypes
+                .Where(e => 
+                e.IsSubclassOf(typeof(Page)) ||
+                e.IsSubclassOf(typeof(ViewModel))))
+            {
+                ContainerBuilder.RegisterType(type.AsType());
+            };
+        }
+        private void FinishInitialization()
         {
             throw new NotImplementedException();
         }
