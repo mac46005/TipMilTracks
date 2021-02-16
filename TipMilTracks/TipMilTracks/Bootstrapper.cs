d52fb4913570e77411bdecd01759bf2e8e1194aa
Marco Preciado <mac46005@gmail.com>
2021-02-15 20:24:57 -0600

    configuring Bootstrapper

---
+++ TipMilTracks/TipMilTracks/Bootstrapper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TipMilTracks
+{
+    public abstract class Bootstrapper
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+            FinishInitialization();
+        }
+
+        private void FinishInitialization()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
